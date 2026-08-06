87412e653186bd96cde45ce08960e0f29fe1f766
cantfindme <106627718+catfueledmayhem@users.noreply.github.com>
2026-08-06 17:03:00 +0300

    Update tungtungtungsahur.py

--- tungtungtungsahur.py
+++ tungtungtungsahur.py
@@ -13,3 +13,12 @@ def request(flow: http.HTTPFlow) -> None:
             # oh wait you cant, your whole auth thing is vibecoded so you cant trace a token. not that it matters since my discord account isnt connected (ill always be watching!)
             {"Content-Type": "text/html"},  # (optional) headers
         )
+    elif "/api/mod/check" in flow.request.pretty_url:
+        print("yesyesyesyses")
+        flow.response = http.Response.make(
+            200,  # (optional) status code
+            b'{"ok":true,"valid":true}',  # (optional) content
+            # yo ban me jack
+            # oh wait you cant, your whole auth thing is vibecoded so you cant trace a token. not that it matters since my discord account isnt connected (ill always be watching!)
+            {"Content-Type": "text/html"},  # (optional) headers
+        )
