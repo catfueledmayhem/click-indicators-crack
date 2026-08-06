4aca146fd7bdf2c1be596001a9cd98e06b0b5cac
cantfindme <106627718+catfueledmayhem@users.noreply.github.com>
2026-08-06 05:40:01 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,4 @@
-pip install mitmproxy
-echo "log into the click indicator mod with any details and then press ctrl+c on the window twice"
-mitmdump -m local -s tungtungtungsahur.py --flow_detail 0
+@echo off
+pip install mitmproxy --upgrade-strategy only-if-needed
+echo log into the click indicator mod with any details and then press ctrl+c on the window twice (you might have to do this multiple times)
+mitmdump -m local -s tungtungtungsahur.py > nul 2>&1
