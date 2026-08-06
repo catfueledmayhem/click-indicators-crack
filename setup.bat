6720660d5c8fe1465c468a2f8fbc0c7a6aa2458d
cantfindme <106627718+catfueledmayhem@users.noreply.github.com>
2026-08-06 19:43:28 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
 @echo off
-pip install mitmproxy --upgrade-strategy only-if-needed
+pip install mitmproxy --upgrade-strategy only-if-needed > nul 2>&1
 echo run this while gd is running, use any details for the login
+echo IMPORTANT!! INSTALL CERTIFICATES FROM http://mitm.it
 mitmdump -m local -s tungtungtungsahur.py > nul 2>&1
