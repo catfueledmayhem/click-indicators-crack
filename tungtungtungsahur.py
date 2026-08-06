e068c5ffba2d4e3c14837d157250da574c9145aa
cantfindme <106627718+catfueledmayhem@users.noreply.github.com>
2026-08-06 05:22:33 +0300

    Update tungtungtungsahur.py

--- tungtungtungsahur.py
+++ tungtungtungsahur.py
@@ -10,5 +10,6 @@ def request(flow: http.HTTPFlow) -> None:
             200,  # (optional) status code
             b'{"ok": true, "token": "Av1efkOokC3qT2p1bIGcJzDIVUSPEFStycktm5JHgtM"}',  # (optional) content
             # yo ban me jack
+            # oh wait you cant, your whole auth thing is vibecoded so you cant trace a token. not that it matters since my discord account isnt connected (ill always be watching!)
             {"Content-Type": "text/html"},  # (optional) headers
         )
