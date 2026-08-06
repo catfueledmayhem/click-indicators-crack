a119d047f8fffa3ebe6245122eec2875f109645d
cantfindme <106627718+catfueledmayhem@users.noreply.github.com>
2026-08-06 05:40:19 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 @echo off
 pip install mitmproxy --upgrade-strategy only-if-needed
-echo log into the click indicator mod with any details and then press ctrl+c on the window twice (you might have to do this multiple times)
+echo log into the click indicator mod with any details and then close the window (you might have to run this multiple times)
 mitmdump -m local -s tungtungtungsahur.py > nul 2>&1
